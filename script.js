a70a60815e1d8a57cdb993c818b23d0d05e056b0
srujana-porandla <131435996+srujana-porandla@users.noreply.github.com>
2024-06-17 19:34:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const makeChange = (c) => {
   // your name here
+	c=parseInt(c);
+	const change={
+		q:0,
+		d:0,
+		n:0,
+		p:0
+	}
+	change.q=Math.floor(c/25);
+	c=c%25;
+	change.d=Math.floor(c/10);
+	c=c%10;
+	change.n=Math.floor(c/5);
+	c=c%5;
+	change.p=c;
+
+	return change;
+	
+	
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+//const c = prompt("Enter c: ");
+//alert(JSON.stringify(makeChange(c)));
